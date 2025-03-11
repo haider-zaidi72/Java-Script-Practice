a4b0ced563cb68539b134861953403d1bd0a28f0
urooj haider <suhz1100@gmail.com>
2025-03-11 03:44:36 -0700

    java-script

---
+++ assignment2.js
@@ -0,0 +1,56 @@
+let username;
+username=('my name is haider zaidi');
+console.log(username);
+
+
+let js=("hello world");
+
+alert('hello world');
+
+let name = "";
+let age = "";
+let education = "";
+
+
+alert('welcome to our site');
+
+alert('enter your name');
+name=prompt('enter your name');
+
+alert('your age');
+age=prompt('enter your age');
+
+alert('your education');
+education=prompt('enter your education');
+
+
+
+console.table([name, age, education]);
+
+let product='pizza';
+let product1='pizz';
+let product2='piz';
+let product3='pi';
+let product4='p';
+console.log(product);
+console.log(product1);
+console.log(product2);
+console.log(product3);
+console.log(product4);
+
+
+let email= ""
+
+alert("My email address is: example@example.com");
+email=prompt('Your email address');
+console.log(email);
+
+let book='I amd trying to learn from the book a smarter way to learn javascript'
+alert(book);
+
+
+console.log(prompt);
+
+let image="image"
+alert(URL);
+
